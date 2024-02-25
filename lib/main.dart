b27072fdeb2d90668679d437a09b599149a1ab41
manar <manarserag77@gnail.com>
2024-02-25 23:50:13 +0200

    initial project. added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo/home/home%20screen.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute:HomeScreen.routeName ,
+      routes: {
+         HomeScreen.routeName: (context)=>HomeScreen(),
+      },
+    );
+  }
+}
+
